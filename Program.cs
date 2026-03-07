75e528448e498582ef38cd6b900620d7cf1328d1
Alizon1234 <escobaralizon29@gmail.com>
2026-03-07 10:47:44 -0400

    Mi primer commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "BIENVENIDOS AL TERRITORIO DE ALIZON");
+app.MapGet("/", () => "BIENVENIDOS AL TERRITORIO DE ALIZON CORTEZ");
 
 app.Run();
